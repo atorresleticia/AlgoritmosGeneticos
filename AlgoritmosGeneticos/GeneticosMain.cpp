c75e64b28e913da943d6830e73967b71412cc1fe
atorresleticia <atorresleticia@gmail.com>
2017-09-27 20:37:34 -0300

    Include args and file to store results

--- AlgoritmosGeneticos/GeneticosMain.cpp
+++ AlgoritmosGeneticos/GeneticosMain.cpp
@@ -10,51 +10,57 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
 	srand(time(NULL));
-	int indice_qualidade;
-	int tamanho_populacao;
-	int tamanho_cromossomo;
-	float taxa_cruzamento;
-	float taxa_mutacao;
-	char elitismo;
-	int geracoes;
 
-	cout << "Numero de geracoes: ";
-	cin >> geracoes;
-	cout << "Tamanho da populacao: ";
-	cin >> tamanho_populacao;
-	cout << "Tamanho do cromossomo: ";
-	cin >> tamanho_cromossomo;
-	cout << "Taxa de cruzamento: ";
-	cin >> taxa_cruzamento;
-	cout << "Taxa de mutacao: ";
-	cin >> taxa_mutacao;
-	//cout << "Qualidade (0 - 100): ";
-	//cin >> indice_qualidade;
-	cout << "Elitismo? [s/n] ";
-	cin >> elitismo;
+	int tamanho_cromossomo = atoi(argv[1]);
+	int tamanho_populacao = atoi(argv[2]);
+	float taxa_cruzamento = atof(argv[3]);
+	float taxa_mutacao = atof(argv[4]);
+	bool elitismo = atoi(argv[5]) == 1 ? true : false;
+
+	cout << "PARAMETROS: " << endl;
+	cout << "Tamanho cromossomo: " << tamanho_cromossomo << endl;
+	cout << "Tamanho populacao: " << tamanho_populacao << endl;
+	cout << "Taxa cruzamento: " << taxa_cruzamento << endl;
+	cout << "Taxa mutacao: " << taxa_mutacao << endl;
+	cout << (elitismo ? "Com elitismo" : "Sem elitismo") << endl;
+
+	/*if (argc != 6)
+	{
+		cout << "Tamanho do cromossomo: ";
+		cin >> tamanho_cromossomo;
+		cout << "Tamanho da populacao: ";
+		cin >> tamanho_populacao;
+		cout << "Taxa de cruzamento: ";
+		cin >> taxa_cruzamento;
+		cout << "Taxa de mutacao: ";
+		cin >> taxa_mutacao;
+		cout << "Elitismo? [s/n] ";
+		cin >> elitismo;
+	}*/
+	
 
 	int g_count = 0;
-	elitismo = elitismo == 's' ? true : false;
+	//elitismo = elitismo == 's' ? true : false;
 	const chrono::high_resolution_clock::time_point init = chrono::high_resolution_clock::now();
 
 	populacao* pop = new populacao(tamanho_populacao, tamanho_cromossomo, true);
 	populacao* nova_p = new populacao(tamanho_populacao, tamanho_cromossomo, true);
 	genetico* gen = new genetico(taxa_mutacao / 100.0, taxa_cruzamento / 100.0, elitismo);
 
-	/*while(pop->get_melhor().get_aptidao() < tamanho_cromossomo*(static_cast<float>(indice_qualidade)/100.0))
-	{
-	pop = gen->evolucao(*pop);
-	g_count++;
-	}*/
-
-	while (g_count < geracoes)
+	while(pop->get_melhor().get_aptidao() < tamanho_cromossomo)
 	{
 		gen->evolucao(*pop, *nova_p);
 		g_count++;
 	}
+
+	/*while (g_count < geracoes)
+	{
+		gen->evolucao(*pop, *nova_p);
+		g_count++;
+	}*/
 
 	const chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
 
@@ -67,6 +73,9 @@ int main()
 
 	aptidao_media /= tamanho_populacao;
 	
+	ofstream info("INFO.txt", ios::app);
+
+	info << "RESULTADOS: " << endl;
 	cout << "Numero de geracoes: " << g_count << endl;
 	cout << "Maior aptidao: " << pop->get_melhor().get_aptidao() << endl;
 	cout << "Aptidao Media: " << aptidao_media << endl;
@@ -74,6 +83,25 @@ int main()
 	cout << "Espaco de busca: " << tamanho_populacao * g_count / pow(2, tamanho_cromossomo) << endl;
 	cout << "Duracao: " << chrono::duration_cast<chrono::duration<double>>(end - init).count() << "s" << endl;
 	cout << endl;
+
+
+	info << "PARAMETROS: " << endl;
+	info << "Tamanho cromossomo: " << tamanho_cromossomo << endl;
+	info << "Tamanho populacao: " << tamanho_populacao << endl;
+	info << "Taxa cruzamento: " << taxa_cruzamento << endl;
+	info << "Taxa mutacao: " << taxa_mutacao << endl;
+	info << (elitismo ? "Com elitismo" : "Sem elitismo") << endl << endl;
+
+	info << "RESULTADOS: " << endl;
+	info << "Numero de geracoes: " << g_count << endl;
+	info << "Maior aptidao encontrada: " << pop->get_melhor().get_aptidao() << endl;
+	info << "Aptidao media: " << aptidao_media << endl;
+	info << "Qualidade da solucao: " << pop->get_melhor().get_aptidao() * 100.0 / tamanho_cromossomo << "%" << endl;
+	info << "Espaco de busca: " << tamanho_populacao * g_count / pow(2, tamanho_cromossomo) << endl;
+	info << "Duracao: " << chrono::duration_cast<chrono::duration<double>>(end - init).count() << "s" << endl;
+	info << "-------------------------------" << endl << endl;
+
+	info.close();
 
 	return 0;
 }
---
+++ AlgoritmosGeneticos/run.bat
@@ -0,0 +1,26 @@
+@echo off
+
+echo Executando [ 512 50 80 0.5 Elitismo ]
+Genetico.exe 512 50 80 0.5 1
+
+echo Executando [ 512 50 40 0.5 Elitismo ]
+Genetico.exe 512 50 40 0.5 1
+
+echo Executando [ 512 50 80 2 Elitismo ]
+Genetico.exe 512 50 80 2 1
+
+echo Executando [ 512 50 40 2 Elitismo ]
+Genetico.exe 512 50 40 2 1
+
+echo Executando [ 512 200 80 0.5 Elitismo ]
+Genetico.exe 512 200 80 0.5 1
+
+echo Executando [ 512 200 40 0.5 Elitismo ]
+Genetico.exe 512 200 40 0.5 1
+
+echo Executando [ 512 200 80 2 Elitismo ]
+Genetico.exe 512 200 80 2 1
+
+echo Executando [ 512 200 40 2 Elitismo ]
+Genetico.exe 512 200 40 2 1
+
